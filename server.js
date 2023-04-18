9e5269aadf94603e433e210813cd2929ca6d3aec
Naman Sharma <96349715+namansharma3007@users.noreply.github.com>
2023-04-18 20:17:00 +0530

    changes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 
 
 
-app.use(cors());
+// app.use(cors());
 
 
 app.use(
@@ -23,13 +23,13 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-// app.use(
-// 	cors({
-// 		origin: process.env.CLIENT_URL,
-// 		methods: "GET,POST,PUT,DELETE",
-// 		credentials: true,
-// 	})
-// );
+app.use(
+	cors({
+		origin: process.env.CLIENT_URL,
+		methods: "GET,POST,PUT,DELETE",
+		credentials: true,
+	})
+);
 
 
 app.use("/auth", authRoute);
