adb3d0601ff3d1972869aa8c31d8b98c4521fc51
Naman Sharma <96349715+namansharma3007@users.noreply.github.com>
2023-04-20 01:18:13 +0530

    changes

--- server.js
+++ server.js
@@ -33,20 +33,5 @@ app.use(passport.session());
 app.use("/auth", authRoute);
 
 
-app.use((req, res, next) => {
-	const allowedOrigins = ["http://localhost:3000", "https://board-dashboard-listed.netlify.app"];
-	const origin = req.headers.origin;
-	if (allowedOrigins.includes(origin)) {
-		res.setHeader("Access-Control-Allow-Origin", origin);
-	}
-	res.header(
-		"Access-Control-Allow-Headers",
-		"Origin, X-Requested-With, Content-Type, Accept"
-	);
-	res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-	res.header("Access-Control-Allow-Credentials", true);
-	next();
-});
-
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listenting on port ${port}...`));
