702526132c287d56db0826314dba2ab72a06b0fe
Naman Sharma <96349715+namansharma3007@users.noreply.github.com>
2023-04-20 00:56:59 +0530

    changes

--- server.js
+++ server.js
@@ -10,8 +10,7 @@ const app = express();
 app.use(
 	cors({
 		// origin: "http://localhost:3000",
-		// origin: "https://board-dashboard-listed.netlify.app",
-		origin: "*",
+		origin: "https://board-dashboard-listed.netlify.app",
 		methods: "GET,POST,PUT,DELETE",
 		credentials: true,
 	})
