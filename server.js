41c69892af839759e7288965b11b7fbc09e0203a
Naman Sharma <96349715+namansharma3007@users.noreply.github.com>
2023-04-20 17:29:59 +0530

    changes

--- server.js
+++ server.js
@@ -9,8 +9,7 @@ const app = express();
 
 app.use(
 	cors({
-		// origin: "http://localhost:3000",
-		origin: "https://board-dashboard-listed.vercel.app",
+		origin: "http://localhost:3000",
 		methods: "GET,POST,PUT,DELETE",
 		credentials: true,
 	})
