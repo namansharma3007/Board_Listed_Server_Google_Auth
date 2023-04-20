6ef6628f14d6d70f47bf1a593bc537222c5fad8c
Naman Sharma <96349715+namansharma3007@users.noreply.github.com>
2023-04-20 09:39:26 +0530

    adding access control origin

--- server.js
+++ server.js
@@ -29,6 +29,14 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "https://board-dashboard-listed.netlify.app");
+	res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+	res.setHeader("Access-Control-Allow-Credentials", "true");
+	next();
+});
+
+
 
 app.use("/auth", authRoute);
 
